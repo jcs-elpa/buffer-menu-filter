ec31da0720ad3a0665b893ea5552f6337943c96c
Jen-Chieh Shen <jcs090218@gmail.com>
2022-02-19 15:26:14 +0800

    Add src

---
+++ buffer-menu-filter.el
@@ -0,0 +1,44 @@
+;;; buffer-menu-filter.el --- Filter buffer-menu items  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Created date 2022-02-19 15:21:40
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Filter buffer-menu items.
+;; Keyword: buffer menu filter
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs-elpa/buffer-menu-filter
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Filter buffer-menu items.
+;;
+
+;;; Code:
+
+(defgroup buffer-menu-filter nil
+  "Filter buffer-menu items."
+  :prefix "buffer-menu-filter-"
+  :group 'convenience
+  :link '(url-link :tag "Repository" "https://github.com/jcs-elpa/buffer-menu-filter"))
+
+
+
+(provide 'buffer-menu-filter)
+;;; buffer-menu-filter.el ends here
